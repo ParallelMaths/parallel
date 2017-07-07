f0daadf098f2afcd6fa6af5007c5a43f39da7e71
Philipp Legner <philipp@mathigon.org>
2017-07-07 08:43:45 +0100

    Add password reset form

--- src/scripts.js
+++ src/scripts.js
@@ -119,10 +119,35 @@ function ready() {
   const login = {
     error: null,
     showDropdown: false,
+    reset: false,
     toggleDropdown() {
       login.showDropdown = !login.showDropdown;
     },
+    toggleReset() {
+      login.error = null;
+      login.reset = !login.reset;
+    },
     submit(e) {
+      if (login.reset) {
+        e.preventDefault();
+        login.error = null;
+        fbAuth.sendPasswordResetEmail(login.email)
+          .then(function() {
+            login.error = 'We\'ve sent you an email with instructions how to reset your password.';
+          })
+          .catch(function(error) {
+            switch(error.code) {
+              case 'auth/invalid-email':
+                return login.error = 'This email address is invalid.';
+              case 'auth/user-not-found':
+                return login.error = 'There is no account with this email address.';
+              default:
+                return login.error = 'Something went wrong. Please try again!';
+            }
+          });
+        return;
+      }
+
       e.preventDefault();
       login.error = null;
       fbAuth.signInWithEmailAndPassword(login.email, login.password)
