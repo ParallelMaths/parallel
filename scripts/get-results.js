2a03c2c930087ce14790297860781f3e7ce1966a
Philipp Legner <philipp@mathigon.org>
2018-09-26 22:23:58 +0100

    Export separate results spreadsheet for TTS classes.

--- scripts/get-results.js
+++ scripts/get-results.js
@@ -4,6 +4,33 @@ const fb = require('firebase-admin');
 const serviceAccount = require('../private/service-account.json');
 const yaml = require('yamljs');
 
+const extract = [
+  "gaqdu",
+  "mg4414",
+  "h343j",
+  "qw6ok",
+  "fd5ryc",
+  "9h9mga",
+  "frwics",
+  "muigu",
+  "14lq9e",
+  "onwaes",
+  "agv1u",
+  "ahupq9",
+  "et857w",
+  "9knp4",
+  "ojiy8q",
+  "a896w",
+  "cb8sj",
+  "yjhx14",
+  "364052",
+  "3to7uh",
+  "nww75",
+  "2c7db",
+  "9mhdd",
+  "t1bmm"
+];
+
 fb.initializeApp({
   credential: fb.credential.cert(serviceAccount),
   databaseURL: 'https://parallel-cf800.firebaseio.com'
@@ -39,22 +66,27 @@ async function run() {
         emailMap[u],
         `"${users[u].schoolName || ''}"`,
         users[u].teacherCode,
-        users[u].country,
+        `"${users[u].country}"`,
         answer.submitted ? 1 : '',
         answer.score || 0
       ];
 
       for (let key of Object.keys(answer)) if (titles.indexOf(key) <= 0) titles.push(key);
-      for (let i=5; i<titles.length; ++i) d.push(answer[titles[i]] || '""');
+      for (let i=7; i<titles.length; ++i) d.push(answer[titles[i]] || '""');
 
       data.push(d);
     }
 
-    data.sort((a, b) => { return a[4] < b[4] ? 1 : a[4] > b[4] ? -1 : 0; });
+    data.sort((a, b) => { return a[6] < b[6] ? 1 : a[6] > b[6] ? -1 : 0; });
 
     const titlesStr = titles.join(',') + '\n';
     const rowsStr = data.map(d => d.join(',')).join('\n');
     fs.writeFileSync(path.join(__dirname, `../private/results-${p.url}.csv`), titlesStr + rowsStr);
+
+    const data1 = data.filter(d => extract.indexOf(d[3]) >= 0);
+    data1.sort((a, b) => { return a[3] < b[3] ? 1 : a[3] > b[3] ? -1 : 0; });
+    const rowsStr1 = data1.map(d => d.join(',')).join('\n');
+    fs.writeFileSync(path.join(__dirname, `../private/results-${p.url}-tts.csv`), titlesStr + rowsStr1);
   }
 
   // ---------------------------------------------------------------------------
