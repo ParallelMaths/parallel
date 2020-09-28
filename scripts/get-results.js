55ad59eff970d816de90913967a1bed51c5bafd1
Philipp Legner <philipp@mathigon.org>
2020-09-28 21:54:08 +0200

    Update get-results script

--- scripts/get-results.js
+++ scripts/get-results.js
@@ -38,10 +38,12 @@ async function run() {
   const emailMap = {};
   for (let a of accounts) emailMap[a.localId] = a.email;
 
+  const summary = {};
+
   // ---------------------------------------------------------------------------
 
   for (let p of pages) {
-    const titles = ['name','email','school','submitted','time/min','score'];
+    const titles = ['name', 'email', 'school', 'time (min)', 'score'];
     const data = [];
 
     for (let u of Object.keys(users)) {
@@ -49,19 +51,24 @@ async function run() {
 
       const user = users[u];
       const answer = users[u].answers[p.url];
-      if (!answer || answer.archive || !inTimeRange(answer)) continue;
+      if (!answer || answer.archive || !answer.submitted || !inTimeRange(answer)) continue;
+
+      const name = p.url.split('-').map(i => +i).slice(0, 2).join('-');
+      summary[name] = (summary[name] || 0) + 1;
 
       const d = [
         user.first + ' ' + user.last,
         emailMap[u],
         `"${users[u].schoolName || ''}"`,
-        answer.submitted ? 1 : '',
         Math.round((answer.time - answer.firstAnswer) / 1000 / 60) || '',
-        answer.score || ''
+        answer.score ? (answer.score + '%') : ''
       ];
 
-      for (let key of Object.keys(answer)) if (titles.indexOf(key) <= 0) titles.push(key);
-      for (let i=7; i<titles.length; ++i) d.push(answer[titles[i]] || '""');
+      for (let key of Object.keys(answer)) {
+        if (['firstAnswer', 'points', 'time', 'total', 'submitted'].includes(key)) continue;
+        if (titles.indexOf(key) <= 0) titles.push(key);
+      }
+      for (let i=5; i<titles.length; ++i) d.push(answer[titles[i]] || '""');
 
       data.push(d);
     }
@@ -92,6 +99,17 @@ async function run() {
 
   const str = [titles, ...data].map(d => d.join(',')).join('\n');
   fs.writeFileSync(path.join(__dirname, `../private/results.csv`), str);
+
+  // ---------------------------------------------------------------------------
+
+  const weeks = Math.max(...Object.keys(summary).map(c => +c.split('-')[1]));
+  const rows = [['', 'Year 7', 'Year 8', 'Year 9', 'Year 10', 'Year 11']];
+  for (let w = 1; w <= weeks; ++w) {
+    rows.push([`Week ${w}`, ...[7, 8, 9, 10, 11].map(i => summary[`${i}-${w}`])])
+  }
+
+  const str1 = rows.map(d => d.join(',')).join('\n');
+  fs.writeFileSync(path.join(__dirname, `../private/summary.csv`), str1);
 
   process.exit();
 }
