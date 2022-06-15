bbcc83b24ceee3f09c096c4c3cf234e4851b2b39
Drew McMillan <drew.mcmillan@thetrainline.com>
2022-06-16 01:13:29 +0200

    weird hack to fix firebase issue

--- scripts/get-users.js
+++ scripts/get-users.js
@@ -3,19 +3,36 @@ const path = require('path');
 const fb = require('firebase-admin');
 const serviceAccount = require('../private/service-account.json');
 
+const loadAllUserData = async (limit, offset, users) => {
+  const newUsers = await fb.firestore().collection('users').offset(offset).limit(limit).get().then(data => 
+    data.docs.reduce((acc, doc) => {
+    acc[doc.id] = doc.data();
+    return acc;
+  }, {}));
+
+  const newUserCount = Object.keys(newUsers).length;
+
+  if (Object.keys(newUsers).length > 1) {
+    console.log('Found', newUserCount, 'users');
+    return loadAllUserData(limit, offset + limit, {...users, ...newUsers})
+  }
+
+  return {...users, ...newUsers};
+}
+
 fb.initializeApp({
   credential: fb.credential.cert(serviceAccount),
   databaseURL: 'https://parallel-cf800.firebaseio.com'
 });
 
-fb.firestore().collection('users').get().then(data => {
-  const users = data.docs.reduce((acc, doc) => {
-    acc[doc.id] = doc.data();
-    return acc;
-  }, {});
 
+const limit = 10000;
+
+const run = async () => {
   const file = path.join(__dirname, `../private/tmp-users.json`);
   const accounts = JSON.parse(fs.readFileSync(file)).users;
+
+  let users = await loadAllUserData(limit, 0, {});
 
   let teachers = 'email,first,last,schoolName,phoneNumber,postCode,teacherCode\n';
   let students = 'email,first,last,level,birthYear,schoolName,teacherCode,guardianEmail\n';
@@ -34,4 +51,6 @@ fb.firestore().collection('users').get().then(data => {
   fs.writeFileSync(path.join(__dirname, `../private/students.csv`), students);
 
   process.exit();
-});
+}
+
+run();
