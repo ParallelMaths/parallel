38c6a436858950c529ac6c9416db06d5ae33dad0
Drew McMillan <drew@mcmill.co.uk>
2022-09-14 00:17:12 +0100

    Fix level 1 sidebar issue

--- static/scripts/user.js
+++ static/scripts/user.js
@@ -121,8 +121,6 @@ export default function() {
     }
   });
 
-  const cachedLevel = document.cookie.match(/level=(year[7-9])/);
-
   const loginForm = {error: null, reset: false};
 
   const editForm = {loading: false, error: '', teacherCodes: []};
@@ -232,32 +230,18 @@ export default function() {
     }
   }
 
-  let level = cachedLevel ? cachedLevel[1] : (window.USER_LEVEL || 'year6');
+  const cachedLevel = document.cookie.match(/level=(year[6-9])/);
 
-  if (level === 'year5') {
-    level = 'year6'
+  let level = cachedLevel ? cachedLevel[1] : window.USER_LEVEL;
+
+  if(!level || (window.SIDEBAR_LEVELS && !window.SIDEBAR_LEVELS.includes(level))) {
+    level = 'year6';
   }
-
-  if (level === 'year12' || level === 'year13') {
-    level = 'year11'
-  }
-
-
-  const hasSeenYearScopeMessage = document.cookie.match(/yearScopeMessageSeen/);
-  const isTooYoung = window.USER_LEVEL === "year5" || window.USER_LEVEL === "year6";
-  const isTooOld = window.USER_LEVEL === "year12" || window.USER_LEVEL === "year13";
-  const showYearScopeMessage = hasSeenYearScopeMessage ? false : isTooYoung ? 1 : isTooOld ? 2 : false;
 
   const user = {
     level,
     showLogin: false,
-    showYearScopeMessage,
     loginForm, editForm, signupForm, passwordForm, homeEducatorForm,
-
-    hideYearScopeMessage() {
-      user.showYearScopeMessage = false;
-      document.cookie = `yearScopeMessageSeen=1;path=/;max-age=${60 * 60* 24 * 365}`;
-    },
 
     setLevel(l) {
       user.level = l;
