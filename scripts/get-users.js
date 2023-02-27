4dd1780066d5ebb9e93fecf96a2009db21138d3e
Drew McMillan <drew@mcmill.co.uk>
2023-02-27 20:37:08 +0000

    Add guardians export

--- scripts/get-users.js
+++ scripts/get-users.js
@@ -13,6 +13,7 @@ const run = async () => {
 
   let teachers = 'email,first,last,schoolName,phoneNumber,postCode,teacherCode,uniqueId\n';
   let students = 'email,first,last,gender,level,birthYear,birthMonth,schoolName,teacherCode,guardianEmail,uniqueId,studentReference,schoolEmail,studentPanelConsidered,profileComplete,pupilPremium,mastery\n';
+  let guardians = 'email,studentFirst,studentLast,studentUniqueId\n';
 
   for (let a of accounts) {
     const u = users[a.localId];
@@ -22,10 +23,18 @@ const run = async () => {
     } else {
       students += `"${a.email}","${u.first}","${u.last}","${u.gender||''}","${u.level||''}","${u.birthYear||''}","${u.birthMonth||''}","${u.schoolName||''}","${u.teacherCode||''}","${u.guardianEmail||''}","${a.localId||''}","${u.userReference||''}","${u.schoolEmail||''}","${u.studentPanelConsidered||''}","${isProfileComplete(u) ? 1 : 0}","${u.pupilPremium||''}","${u.source==='mastery' ? '1' : '0'}"\n`;
     }
+
+    const newGuardianEmails = (u.emails || []).filter(i => i.type === 'guardian').map(i => i.email)
+    const guardianEmails = [...newGuardianEmails, u.guardianEmail].filter(Boolean);
+
+    for(let g of guardianEmails) {
+      guardians += `"${g}","${u.first}","${u.last}","${a.localId||''}"\n`;
+    }
   }
 
   fs.writeFileSync(path.join(__dirname, `../private/teachers.csv`), teachers);
   fs.writeFileSync(path.join(__dirname, `../private/students.csv`), students);
+  fs.writeFileSync(path.join(__dirname, `../private/guardians.csv`), guardians);
 
   process.exit();
 }
