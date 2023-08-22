edee30e09334706a4dff42be18c4ab801dc6910b
Drew McMillan <drew@mcmill.co.uk>
2023-08-23 00:02:41 +0100

    Update delete users script

--- scripts/delete-old-users.js
+++ scripts/delete-old-users.js
@@ -1,31 +1,101 @@
-const fs = require('fs');
-const path = require('path');
-const fb = require('firebase-admin');
-const serviceAccount = require('../private/service-account.json');
+const fs = require("fs");
+const path = require("path");
+const fb = require("firebase-admin");
+const serviceAccount = require("../private/service-account.json");
 
 fb.initializeApp({
   credential: fb.credential.cert(serviceAccount),
-  databaseURL: 'https://parallel-cf800.firebaseio.com'
+  databaseURL: "https://parallel-cf800.firebaseio.com",
 });
 
+const keysToRemove = [
+  "first",
+  "last",
+  "schoolName",
+  "schoolPostcode",
+  "schoolEmail",
+  "school",
+  "postCode",
+  "phoneNumber",
+  "guardianEmail",
+  "emails",
+  "studentPanelGuardianName",
+  "studentPanelGuardianNumber",
+];
+
+const cutoff = new Date("2021-08-22T18:32:06.817Z").getTime();
+
+const file = path.join(__dirname, `../private/tmp-users.json`);
+const accounts = JSON.parse(fs.readFileSync(file)).users;
+
+const userCacheFile = path.join(__dirname, `../private/cache-users.json`);
+const usersAccountData = JSON.parse(fs.readFileSync(userCacheFile)).data;
+
+const usersCollection = fb.firestore().collection("users");
+const usersArchiveCollection = fb.firestore().collection("users-archive");
+
+const deleteAccount = async (id) => {
+  const data = usersAccountData[id];
+
+  if (!data) {
+    console.log("Unable to find user in database", id);
+  } else {
+    const redactedData = Object.fromEntries(
+      Object.entries(data).map(([key, value]) => {
+        if (keysToRemove.includes(key)) {
+          return [key, "__REMOVED__"];
+        }
+        return [key, value];
+      }),
+    );
+
+    // console.log(JSON.stringify(redactedData, null, 4));
+
+    await usersArchiveCollection.doc(id).set(redactedData);
+    await usersCollection.doc(id).delete();
+  }
+
+  await fb.auth().deleteUser(id);
+  console.log("Deleting user", id);
+};
+
 (async () => {
-  const file = path.join(__dirname, `../private/tmp-users.json`);
-  const accounts = JSON.parse(fs.readFileSync(file)).users;
+  console.log("Number of accounts:", accounts.length);
 
-  console.log('Number of accounts:', accounts.length);
+  const oldAccounts = accounts.filter(
+    (a) => new Date(+a.lastSignedInAt).getTime() < cutoff,
+  );
 
-  const oldAccounts = accounts
-    .filter(a => (new Date(+a.lastSignedInAt)).getFullYear() < 2020)
-    .map(a => a.localId);
+  const oldAccountIds = oldAccounts.map((a) => a.localId);
 
-  console.log('Accounts to delete:', oldAccounts.length);
-  let i = 0;
+  console.log("Accounts to delete:", oldAccountIds.length);
 
-  for (const id of oldAccounts) {
-    await fb.auth().deleteUser(id);
-    await fb.database().ref('users/' + id).remove();
-    i += 1;
-    if (i % 100 === 0) console.log(i);
+  // console.log(
+  //   JSON.stringify(
+  //     Object.fromEntries(
+  //       oldAccounts.map((a) => [
+  //         a.localId,
+  //         new Date(+a.lastSignedInAt).toISOString(),
+  //       ]),
+  //     ),
+  //     null,
+  //     4,
+  //   ),
+  // );
+
+  const chunks = [];
+
+  const chunkSize = 9; // Rate limit is max 10 per second
+  for (let i = 0; i < oldAccountIds.length; i += chunkSize) {
+    chunks.push(oldAccountIds.slice(i, i + chunkSize));
+  }
+
+  console.log("Chunks", chunks.length);
+
+  for (const chunk of chunks) {
+    console.log("CHUNK");
+    await new Promise((r) => setTimeout(r, 1200));
+    await Promise.all(chunk.map((id) => deleteAccount(id)));
   }
 
   process.exit();
