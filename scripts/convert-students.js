de487ac40f45e5d31d7867f01861656d6f5239eb
Drew McMillan <drew@mcmill.co.uk>
2023-11-27 11:47:20 +0000

    add student converting script

---
+++ scripts/convert-students.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+const fb = require('firebase-admin');
+const serviceAccount = require('../private/service-account.json');
+
+const STUDENTS = [
+  "foobar@gmail.com"
+];
+
+fb.initializeApp({
+  credential: fb.credential.cert(serviceAccount),
+  databaseURL: 'https://parallel-cf800.firebaseio.com'
+});
+
+const file = path.join(__dirname, `../private/tmp-users.json`);
+const accounts = JSON.parse(fs.readFileSync(file)).users;
+
+async function makeStudent(email) {
+  const a = accounts.find(a => a.email.toLowerCase() === email.toLowerCase());
+  if (!a) return console.error('Could not find ' + email);
+
+  return fb.firestore().collection('users').doc(a.localId).update({
+    schoolName: null,
+    code: null,
+    level: 'year7',
+    birthYear: 2000,
+    teacherCode: null
+  }).catch(error => console.error(error + ',' + email));
+}
+
+const promises = STUDENTS.map(makeStudent);
+Promise.all(promises).then(() => process.exit());
