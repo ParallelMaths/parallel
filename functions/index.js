2455c97adc173f7b3a58a9064a87ddc281615d28
Drew McMillan <drew@mcmill.co.uk>
2024-06-09 23:33:22 +0100

    passwords visible (#70)

--- functions/index.js
+++ functions/index.js
@@ -558,7 +558,7 @@ app.get('/test/:pid', (req, res, next) => {
   let hasPassword = false;
   let passwordIncorrect = false;
 
-  if(!page.password) {
+  if(!page.password || p.answersVisible) {
     hasPassword = true;
   } else if(req.query.p) {
     if(req.query.p == page.password) {
