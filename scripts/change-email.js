f3559393c5ad7922c9acfcbc1d1af5a5637731a6
Drew McMillan <drew@mcmill.co.uk>
2024-06-18 16:49:22 +0100

    add change email script

---
+++ scripts/change-email.js
@@ -0,0 +1,40 @@
+const fb = require("firebase-admin");
+const serviceAccount = require("../private/service-account.json");
+
+// This script will change an account email.
+
+const from = 'euclidstart2001@mcmill.co.uk'
+const to = 'euclidstart2002@mcmill.co.uk'
+
+fb.initializeApp({
+  credential: fb.credential.cert(serviceAccount),
+  databaseURL: "https://parallel-cf800.firebaseio.com",
+});
+
+const run = () =>
+  fb
+    .auth()
+    .getUserByEmail(from)
+    .then(async (user) => {
+      await fb.auth().updateUser(user.uid, {
+        email: to,
+      });
+      console.log(`\nMigrated "${user.email}" email to ${to}`);
+
+      console.log(`\nIf this student is euclid, please send Drew "${user.uid}" and "${to}" to update the euclid account.`)
+    })
+    .catch((err) => {
+      err.message = `${err.message} - ${from}`;
+      throw err;
+    });
+
+run()
+  .then(() => {
+    console.error("\n\n\nSuccess migrating account");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error("\n\n\nError migrating account!");
+    console.error(err.message + "\n");
+    process.exit(1);
+  });
