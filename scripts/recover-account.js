257f274edfb3a21583348eb28f8c229aa56e8815
Michael Marshall <marsh@goodthinkingsociety.org>
2024-10-14 16:03:17 +0100

    Creating homework week 4

--- scripts/recover-account.js
+++ scripts/recover-account.js
@@ -4,8 +4,7 @@ const serviceAccount = require("../private/service-account.json");
 // This script will reset passwords. Used when students are unable to receive emails.
 
 const accountsToRecover = [
-    "DSalter@roundhayschool.com",
-    "DSalter@roundhayschool.com"
+    "lev.jones@bradleystokecs.org.uk"
 ];
 
 const passwordLength = 10;
