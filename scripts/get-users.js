94330b6d490d5e5f3665bdf9b10c288916c94012
Drew McMillan <drew@mcmill.co.uk>
2025-02-02 19:21:51 +0000

    Add academy status to the students csv

--- scripts/get-users.js
+++ scripts/get-users.js
@@ -4,24 +4,37 @@ const fb = require('firebase-admin');
 const serviceAccount = require('../private/service-account.json');
 const downloadUsers = require('./utils/downloadUsers');
 const isProfileComplete = require('../functions/utilities/profileComplete');
+const { getEuclidStatusForUsers } = require('./utils/dynamodb');
 
 const run = async () => {
   let users = await downloadUsers();
 
+  let euclidStatuses = {};
+
+  try {
+    euclidStatuses = await getEuclidStatusForUsers(); 
+  } catch (error) {
+    console.error('🚨🚨 Failed to get euclid statuses');
+    console.error(`🚨🚨 Info: ${error.message}`);
+  }
+
   const file = path.join(__dirname, `../private/tmp-users.json`);
   const accounts = JSON.parse(fs.readFileSync(file)).users;
 
-  let teachers = 'email,first,last,schoolName,phoneNumber,postCode,teacherCode,uniqueId\n';
-  let students = 'email,first,last,gender,level,birthYear,birthMonth,schoolName,teacherCode,guardianEmail,uniqueId,studentReference,schoolEmail,pupilPremium,isEuclidAdmissions\n';
+  let teachers = 'email,first,last,schoolName,phoneNumber,postCode,teacherCode,uniqueId,academyStatus\n';
+  let students = 'email,first,last,gender,level,birthYear,birthMonth,schoolName,teacherCode,guardianEmail,uniqueId,studentReference,schoolEmail,pupilPremium,isEuclidAdmissions,academyStatus\n';
   let guardians = 'email,studentFirst,studentLast,level,studentUniqueId,isEuclidAdmissions\n';
 
   for (let a of accounts) {
     const u = users[a.localId];
     if (!u) continue;
+
+    const euclidStatus = euclidStatuses[a.localId] || 'NONE';
+
     if (u.code) {
-      teachers += `"${a.email}","${u.first}","${u.last}","${u.schoolName||''}","${u.phoneNumber||''}","${u.postCode||''}","${u.code||''}","${a.localId||''}"\n`;
+      teachers += `"${a.email}","${u.first}","${u.last}","${u.schoolName||''}","${u.phoneNumber||''}","${u.postCode||''}","${u.code||''}","${a.localId||''}","${euclidStatus||''}"\n`;
     } else {
-      students += `"${a.email}","${u.first}","${u.last}","${u.gender||''}","${u.level||''}","${u.birthYear||''}","${u.birthMonth||''}","${u.schoolName||''}","${u.teacherCode||''}","${u.guardianEmail||''}","${a.localId||''}","${u.userReference||''}","${u.schoolEmail||''}","${u.pupilPremium||''}","${u.euclidEnrolTimestamp ? '1' : '0'}"\n`;
+      students += `"${a.email}","${u.first}","${u.last}","${u.gender||''}","${u.level||''}","${u.birthYear||''}","${u.birthMonth||''}","${u.schoolName||''}","${u.teacherCode||''}","${u.guardianEmail||''}","${a.localId||''}","${u.userReference||''}","${u.schoolEmail||''}","${u.pupilPremium||''}","${u.euclidEnrolTimestamp ? '1' : '0'}","${euclidStatus||''}"\n`;
     }
 
     const newGuardianEmails = (u.emails || []).filter(i => i.type === 'guardian').map(i => i.email)
--- scripts/utils/dynamodb.js
+++ scripts/utils/dynamodb.js
@@ -1,61 +1,56 @@
 const AWS = require('aws-sdk');
 const path = require('path');
+const fs = require('fs');
 
-// AWS.config.loadFromPath(path.join(__dirname, '../../private/aws-account.json'));
+const awsConfigPath = path.join(__dirname, '../../private/aws-account.json');
+
+if(fs.existsSync(awsConfigPath)) {
+  AWS.config.loadFromPath(awsConfigPath);
+} else {
+  console.error('\n\n\n🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨\n\nAWS config file not found at "private/aws-account.json"\nAcademy information may be missing/incorrect\n\n🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨\n\n\n');
+}
 
 const ddbDocumentClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: 'eu-west-1' });
 
 const getPaginatedTableItemsInner = async (tableName, lastEvaluatedKey) => {
-  try {
-    const params = {
-      TableName: tableName,
-      ExclusiveStartKey: lastEvaluatedKey
-    };
-    const result = await ddbDocumentClient.scan(params).promise();
+  const params = {
+    TableName: tableName,
+    ExclusiveStartKey: lastEvaluatedKey
+  };
+  const result = await ddbDocumentClient.scan(params).promise();
 
-    if (result.LastEvaluatedKey) {
-        console.log('Paginating after', result.Items.length, 'items')
-        const newData = (await getPaginatedTableItemsInner(
-          tableName,
-          result.LastEvaluatedKey
-        ));
-  
-        return [...(result.Items || []), ...newData];
-      }
+  if (result.LastEvaluatedKey) {
+      console.log('Paginating dynamodb after', result.Items.length, 'items')
+      const newData = (await getPaginatedTableItemsInner(
+        tableName,
+        result.LastEvaluatedKey
+      ));
 
-    return result.Items;
-  } catch (error) {
-    console.error(error);
-  }
+      return [...(result.Items || []), ...newData];
+    }
+
+  return result.Items;
 };
 
 const updateItem = async (params) => {
-  try {
-    return ddbDocumentClient.update(params).promise();
-  } catch (error) {
-    console.error(error);
-  }
+  return ddbDocumentClient.update(params).promise();
 }
 
 const getTableItem = async (tableName, id) => {
-  try {
-    const params = {
-      TableName: tableName,
-      Key: {
-        id
-      }
-    };
+  const params = {
+    TableName: tableName,
+    Key: {
+      id
+    }
+  };
 
-    const res = await ddbDocumentClient.get(params).promise();
-    return res.Item;
-  } catch (error) {
-    console.error(error);
-  }
+  const res = await ddbDocumentClient.get(params).promise();
+  return res.Item;
 }
 
 const getPaginatedTableItems = async (tableName) => {
     const items = await getPaginatedTableItemsInner(tableName);
-    console.log('Found', items.length, 'items in dynamodb database table');
+    console.log('Found', items.length, 'items in dynamodb database table', tableName);
     return items;
 }
 
@@ -75,9 +70,19 @@ const getCirclePointsForUsers = async () => {
     }, {})
 }
 
+const getEuclidStatusForUsers = async () => {
+  const data = await getPaginatedTableItems('prod-users');
+
+  return data.reduce((acc, entry) => {
+      acc[entry.id] = entry.euclidStatus
+      return acc;
+  }, {})
+}
+
 module.exports = {
     updateItem,
     getTableItem,
     getCirclePointsForUsers,
-    getPaginatedTableItemsInner
+    getPaginatedTableItemsInner,
+    getEuclidStatusForUsers
 }
