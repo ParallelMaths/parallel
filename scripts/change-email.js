03426254afcba978d8d55174778ab8c3546bf974
Michael Marshall <marsh@goodthinkingsociety.org>
2025-10-21 17:05:40 +0100

    Creating 9aut 5

--- scripts/change-email.js
+++ scripts/change-email.js
@@ -3,8 +3,8 @@ const serviceAccount = require("../private/service-account.json");
 
 // This script will change an account email.
 
-const from = 'aaco50@hotmail.co'
-const to = 'aaco50@hotmail.com'
+const from = 'lalanthi@yahoo.com'
+const to = 'lalanthi026@googlemail.com'
 
 fb.initializeApp({
   credential: fb.credential.cert(serviceAccount),
