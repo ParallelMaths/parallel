d83a59dd451be18bcbf3b953cbfdd05854b333c5
Drew McMillan <drew.mcmillan@thetrainline.com>
2026-02-22 15:15:30 +0000

    Add circle data type admin

--- functions/utilities/user.js
+++ functions/utilities/user.js
@@ -86,6 +86,7 @@ async function getUserFromToken(idToken) {
     ...userData,
     email: decodedIdToken.email || null,
     accountType: decodedIdToken.account_type || null,
+    circlesDataType: decodedIdToken.circles_data_type || null,
     euclidAccountType: decodedIdToken.euclid_type || null,
     privacy: getPrivacyState(decodedIdToken.email, userData),
   }
