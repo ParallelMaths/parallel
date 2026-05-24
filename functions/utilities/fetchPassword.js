9e8dc8aaf71136517b670b3c5f3f52aed8367fa2
Drew McMillan <drew@mcmill.co.uk>
2026-05-24 21:48:38 +0100

    🔧 Continue to implement and debug

--- functions/utilities/fetchPassword.js
+++ functions/utilities/fetchPassword.js
@@ -2,7 +2,7 @@ const OVERRIDE_PASSWORD = 'applemangopear';
 
 async function fetchPassword(pid) {
   try {
-    const response = await fetch(`https://beta.parallel.org.uk/admin/api/password/${encodeURIComponent(pid)}`);
+    const response = await fetch(`https://parallel.org.uk/admin/api/password/${encodeURIComponent(pid)}`);
     const data = await response.json();
     return data.password || OVERRIDE_PASSWORD;
   } catch (e) {
