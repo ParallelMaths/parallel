a5464e8411af4f800854d57219c0ebdbb12fa035
Drew McMillan <drew.mcmillan@thetrainline.com>
2026-07-07 13:43:37 +0100

    ignore student panel

--- functions/utilities/profileComplete.js
+++ functions/utilities/profileComplete.js
@@ -3,7 +3,6 @@ function isProfileComplete(userData) {
     if(!userData.birthMonth) return false;
     if(!userData.birthYear) return false;
     if(!userData.country) return false;
-    if(!userData.studentPanelConsidered) return false;
   
     if(!userData.homeEducated) {
       if(!userData.schoolEmail) return false;
